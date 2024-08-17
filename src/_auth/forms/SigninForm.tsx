c802ef43ad837ab4b8c73c9f62f3a43e78a9be2e
Rahul <rahulhanje0.7@gmail.com>
2024-08-17 18:02:21 +0530

    Sign** logo size done

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -66,7 +66,7 @@ function SigninForm() {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="\assets\images\logo.svg" />
+        <img src="\assets\images\logo.svg" width="250" height="100" />
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Login to Your account</h2>
         <p className="text-light-3 small-medium md:base-regular">Welcome back please enter your details</p>
 
