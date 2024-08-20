1be560d4bd792611a53d4a9e0ccb617b2077b3a1
Rahul <rahulhanje0.7@gmail.com>
2024-08-20 17:29:14 +0530

    Post fetch limit is increased

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -199,7 +199,7 @@ export async function getRecentPosts() {
   const posts = await databases.listDocuments(
     appwriteConfig.databaseId,
     appwriteConfig.postCollectionId,
-    [Query.orderDesc("$createdAt"), Query.limit(20)]
+    [Query.orderDesc("$createdAt"), Query.limit(30)]
   )
   if (!posts) {
     throw Error;
