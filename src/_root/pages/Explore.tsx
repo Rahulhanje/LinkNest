37890afc664d98a9a43a65d6d01a3281615cc0d5
Rahul <rahulhanje0.7@gmail.com>
2024-08-21 20:09:13 +0530

    Removed people page error

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -35,7 +35,7 @@ function Explore() {
 
 
   return (
-    <div className="explore-container  ">
+    <div className="explore-container">
     <div className="explore-inner_container">
       <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
       <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
